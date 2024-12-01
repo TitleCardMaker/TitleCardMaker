2b26436726e09bbb1e09560686ade92851a1d5e3
Collin Heist <collinheist@gmail.com>
2024-11-30 20:46:15 -0700

    [132] Revise log message for Episode ID edits

--- app/models/episode.py
+++ app/models/episode.py
@@ -335,7 +335,8 @@ class Episode(Base):
         changed = False
         for id_type, id_ in info.ids.items():
             if id_ and getattr(self, id_type) != id_:
-                log.debug(f'{self}.{id_type} | {getattr(self, id_type)} -> {id_}')
+                log.debug(f'Episode[{self.id}].{id_type} = '
+                          f'{getattr(self, id_type)} -> {id_}')
                 setattr(self, id_type, id_)
                 changed = True
 
