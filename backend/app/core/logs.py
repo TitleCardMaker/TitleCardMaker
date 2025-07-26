8a364d837ece12261dfa63f9e68d33fc7beb96d2
Collin Heist <collinheist@gmail.com>
2025-07-25 23:59:06 -0600

    [139] Update logs.py

--- backend/app/core/logs.py
+++ backend/app/core/logs.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from logging import Logger
 
-from app.dependencies import get_database
+from app.dependencies import get_log_database
 from app.logging.logger import log
 from app.logging.models import Log
 from app.settings import settings
@@ -10,7 +10,7 @@ from app.settings import settings
 def clear_log_data(*, log: Logger = log) -> None:
     """Clear old logs from the database."""
 
-    with next(get_database()) as db:
+    with next(get_log_database()) as db:
         cutoff_date = (
             datetime.now()
             - timedelta(days=settings.LOG_RETENTION_DAYS)
