070ca028bd1444131f3c675c3ef952a07e7ee0e8
Collin Heist <collinheist@gmail.com>
2025-08-22 15:33:50 -0600

    [306] Update pagination.py

--- backend/app/db/pagination.py
+++ backend/app/db/pagination.py
@@ -1,4 +1,4 @@
-from fastapi_pagination import Page
+from fastapi_pagination import Page as BasePage
 from fastapi_pagination.customization import (
     CustomizedPage,
     UseName,
@@ -8,7 +8,7 @@ from fastapi_pagination.customization import (
 
 # Default Page arguments used for paginated returns
 Page = CustomizedPage[
-    Page,
+    BasePage,
     UseName('Page'),
     UseParamsFields(size=50),
 ]
